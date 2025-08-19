535486d3c46d332af246941f33de890e710c19e8
ARAFAT AMAN ALIM 🇮🇳 <76907565+Arafat-alim@users.noreply.github.com>
2025-08-20 02:00:50 +0530

    ✅ Bottom Tab Navigatior creaated.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 import DevApp from './src/dev/DevApp';
+import 'react-native-gesture-handler';
 
 function Root() {
   if (__DEV__) {
