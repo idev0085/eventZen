7f93291c0691863e9200259330476a9240b65d64
ARAFAT AMAN ALIM 🇮🇳 <76907565+Arafat-alim@users.noreply.github.com>
2025-08-21 01:14:00 +0530

    🔨 Working on Home Drawer Navigation component.

---
+++ src/navigation/HomeDrawerNavigator.tsx
@@ -0,0 +1,47 @@
+// src/navigation/HomeDrawerNavigator.tsx
+import React from 'react';
+import { View } from 'react-native'; // View is not actually used here, can be removed
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import HomeScreen from '../screens/HomeScreen';
+import CustomSideBar from '../screens/CustomSidebar';
+import { COLORS } from '../utils/constants';
+import DrawerMenuIcon from '../../assets/svg/svgComponents/DrawerMenuIcon';
+import { useNavigation } from '@react-navigation/native';
+
+const Drawer = createDrawerNavigator();
+
+const HomeDrawerNavigator = () => {
+  const navigation = useNavigation();
+  return (
+    <Drawer.Navigator
+      initialRouteName="HomeContent"
+      drawerContent={props => <CustomSideBar {...props} />}
+      screenOptions={{
+        headerShown: true,
+        headerTitleAlign: 'center',
+        headerStyle: { backgroundColor: COLORS.primary },
+        headerTintColor: COLORS.white,
+        drawerStyle: {
+          backgroundColor: COLORS.background,
+          width: '95%',
+        },
+        headerLeft: () => (
+          <DrawerMenuIcon
+            width={30}
+            height={30}
+            style={{ marginLeft: 15 }}
+            onPress={() => navigation.toggleDrawer()}
+          />
+        ),
+      }}
+    >
+      <Drawer.Screen
+        name="HomeContent"
+        component={HomeScreen}
+        options={{ title: 'Home' }}
+      />
+    </Drawer.Navigator>
+  );
+};
+
+export default HomeDrawerNavigator;
