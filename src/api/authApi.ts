fcc2d9939dfde13cfe18090768011ee8f74b74e3
Arafat-alim <arafat97alim@gmail.com>
2025-08-30 15:08:51 +0530

    ✔️ AuthAPI: Request OTP added.

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -3,13 +3,30 @@ import {
   VerifyOtpResponseSchema,
   UserProfileResponseSchema,
   TUser,
+  RequestOtpResponseSchema,
 } from '../schemas/authSchemas';
 import { apiClient } from './apiClient';
-
+import Toast from 'react-native-simple-toast';
 interface OtpPayload {
   email: string;
   otp: string;
 }
+
+export const requestOtp = async (payload: {
+  email: string;
+}): Promise<string> => {
+  const { data } = await apiClient.post('/auth/login', payload);
+  const validateData = RequestOtpResponseSchema.parse(data);
+
+  if (__DEV__) {
+    console.log(
+      `DEV ONLY: OTP for ${validateData.data.email} is ${validateData.data.otp}`,
+    );
+    Toast.show(`OTP: ${validateData.data.otp}`, Toast.LONG);
+  }
+
+  return validateData.data.email;
+};
 
 export const verifyOtp = async (payload: OtpPayload): Promise<string> => {
   const { data } = await apiClient.post('/auth/verify-otp', payload);
