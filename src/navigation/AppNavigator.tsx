1be990e8343bb15a530e5c6d1e3c0c6782448f82
ARAFAT AMAN ALIM 🇮🇳 <76907565+Arafat-alim@users.noreply.github.com>
2025-09-01 03:16:12 +0530

    ✔️ Login Screen route added in the appnavigator.

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import BottomTabNavigator from './BottomTabNavigator';
 import CompanyDetailsScreen from '../screens/CompanyDetails';
+import LoginScreen from '../screens/LoginScreen';
 
 const AppStack = createStackNavigator();
 function AppNavigator() {
@@ -9,6 +10,7 @@ function AppNavigator() {
     <AppStack.Navigator screenOptions={{ headerShown: false }}>
       <AppStack.Screen name="MainTabs" component={BottomTabNavigator} />
       <AppStack.Screen name="CompanyDetails" component={CompanyDetailsScreen} />
+      <AppStack.Screen name="LoginScreen" component={LoginScreen} />
     </AppStack.Navigator>
   );
 }
