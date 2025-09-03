2175912d7f11488b657699b680c625baa3443ae5
xigmasoftsin <xigmasofts.in@gmail.com>
2025-09-04 01:18:03 +0530

    Home UI

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useCallback } from 'react';
-import { ScrollView, StyleSheet, View } from 'react-native';
+import { ScrollView, StyleSheet, Alert, View, Text } from 'react-native';
 import HomeHeader from '../components/homeHeader';
 import QuickActionMenu from '../components/quickActionMenu';
 import UpcomingEvent from '../components/upcomingEvent';
@@ -7,6 +7,89 @@ import ConnectionsCard from '../components/connectionCard';
 import { GENERATED_CONNECTIONS } from '../utils/constants';
 import Toast from 'react-native-simple-toast';
 import YoutubePlayer from 'react-native-youtube-iframe';
+import SessionListItem from '../components/sessionListItem';
+import Card from '../components/card';
+import { COLORS, TEXT_SIZES } from '../utils/constants';
+const HomeSessions = () => {
+  return (
+    <>
+      <View style={styles.sessionHeadWrapper}>
+        <Text style={styles.label}>Sessions</Text>
+        <Text
+          style={styles.labelViewAll}
+          onPress={() => Alert.alert('View All')}
+        >
+          See more
+        </Text>
+      </View>
+      <Card style={styles.card}>
+        <SessionListItem
+          title="Opening Keynote: The Future of Innovation"
+          time="10:00 AM - 11:00 AM"
+          onPress={() => Alert.alert('Item Pressed')}
+          speakers={[
+            {
+              name: 'Dr. A',
+              designation: 'Chief Innovation Officer',
+              company: 'Innovatech Solutions',
+            },
+          ]}
+          workshopNo="Workshop NO : 01"
+          status="Ongoing"
+        />
+      </Card>
+      <Card style={styles.card}>
+        <SessionListItem
+          title="Opening Keynote: The Future of Innovation"
+          time="10:00 AM - 11:00 AM"
+          onPress={() => Alert.alert('Item Pressed')}
+          speakers={[
+            {
+              name: 'Dr. A',
+              designation: 'Chief Innovation Officer',
+              company: 'Innovatech Solutions',
+            },
+          ]}
+          workshopNo="Workshop NO : 01"
+          status="Ongoing"
+        />
+      </Card>
+      <Card style={styles.card}>
+        <SessionListItem
+          title="Opening Keynote: The Future of Innovation"
+          time="10:00 AM - 11:00 AM"
+          onPress={() => Alert.alert('Item Pressed')}
+          speakers={[
+            {
+              name: 'Dr. A',
+              designation: 'Chief Innovation Officer',
+              company: 'Innovatech Solutions',
+            },
+          ]}
+          workshopNo="Workshop NO : 01"
+          status="Ongoing"
+        />
+      </Card>
+      <Card style={styles.card}>
+        <SessionListItem
+          title="Opening Keynote: The Future of Innovation"
+          time="10:00 AM - 11:00 AM"
+          onPress={() => Alert.alert('Item Pressed')}
+          speakers={[
+            {
+              name: 'Dr. A',
+              designation: 'Chief Innovation Officer',
+              company: 'Innovatech Solutions',
+            },
+          ]}
+          workshopNo="Workshop NO : 01"
+          status="Ongoing"
+        />
+      </Card>
+    </>
+  );
+};
+
 const HomeScreen = () => {
   const [playing, setPlaying] = useState(false);
   const onStateChange = useCallback(state => {
@@ -38,6 +121,7 @@ const HomeScreen = () => {
       />
       <UpcomingEvent eventDate={new Date(2025, 8, 4, 2, 31, 0)} />
       <QuickActionMenu />
+      <HomeSessions />
       <ConnectionsCard
         connections={GENERATED_CONNECTIONS}
         onStartNetworking={handleOnStartNetworking}
@@ -51,5 +135,38 @@ export default HomeScreen;
 const styles = StyleSheet.create({
   homeScreenContainer: {
     flex: 1,
+  },
+  card: {
+    marginHorizontal: 10,
+    alignSelf: 'center',
+    width: '90%',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 3,
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: COLORS.white,
+  },
+  label: {
+    fontSize: TEXT_SIZES.md,
+    fontFamily: 'Roboto-Bold',
+    color: COLORS.black,
+  },
+  labelViewAll: {
+    fontSize: TEXT_SIZES.md,
+    fontFamily: 'Roboto-Bold',
+    color: COLORS.primary,
+  },
+  sessionHeadWrapper: {
+    marginTop: 10,
+    marginBottom: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingLeft: 20,
+    paddingRight: 20,
   },
 });
