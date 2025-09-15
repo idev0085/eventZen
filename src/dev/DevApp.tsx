1d7831be3b591fc1724c9b4a157d7aa2ec0a72c4
ARAFAT AMAN ALIM 🇮🇳 <76907565+Arafat-alim@users.noreply.github.com>
2025-09-16 03:22:25 +0530

    ➕ Working on cards.

--- src/dev/DevApp.tsx
+++ src/dev/DevApp.tsx
@@ -3,23 +3,14 @@ import { View, Button, Text, SafeAreaView } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import LoginScreen from '../screens/LoginScreen';
-import FilterDropDown from '../components/filterDropDown';
 import BootSplash from 'react-native-bootsplash';
+import FileUploadCard from '../components/fileUploadCard';
 
 const Stack = createStackNavigator();
-const data = [
-  'Data',
-  'Startup',
-  'Technology',
-  'Cloud',
-  'Business',
-  'Security',
-  'Networking',
-];
 
 const TestScreen = ({ navigation }: any) => (
   <SafeAreaView style={{ flex: 1, marginTop: 50 }}>
-    <FilterDropDown options={data} label="Tag" />
+    <FileUploadCard />
   </SafeAreaView>
 );
 
