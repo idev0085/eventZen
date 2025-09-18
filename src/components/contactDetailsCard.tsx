f4f83a626dcaa8e5a57e0120b85cd2dce922e894
ARAFAT AMAN ALIM 🇮🇳 <76907565+Arafat-alim@users.noreply.github.com>
2025-09-19 02:21:37 +0530

    ⬆️ Contact Details card updated.

--- src/components/contactDetailsCard.tsx
+++ src/components/contactDetailsCard.tsx
@@ -1,104 +1,95 @@
 import React from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  Linking,
-} from 'react-native';
-
-interface Detail {
-  label: string;
-  value: string;
-  url?: string;
-}
+import { StyleSheet, Text, View } from 'react-native';
+import CustomText from './ui/text';
+import { COLORS, TEXT_SIZES } from '../utils/constants';
 
 interface ContactDetailsCardProps {
-  details: {
-    email: string;
-    phone: string;
-    address: string;
-    website: string;
-  };
+  email: string;
+  phone: string;
+  address: string;
+  website?: string;
+  enablePhoneIcon?: boolean;
+  enableEmailIcon?: boolean;
+  onPressPhone?: () => void;
+  onPressIcon?: () => void;
+  onPressWebsite?: () => void;
 }
 
-const DetailItem: React.FC<Detail> = ({ label, value, url }) => {
-  const handlePress = () => {
-    if (url) {
-      Linking.openURL(url).catch(err =>
-        console.error("Couldn't load page", err),
-      );
-    }
-  };
-
+const ContactDetailsCard = ({
+  email,
+  website,
+  phone,
+  address,
+  enableEmailIcon,
+  enablePhoneIcon,
+  onPressPhone,
+  onPressIcon,
+  onPressWebsite,
+}: ContactDetailsCardProps) => {
   return (
-    <View style={styles.itemContainer}>
-      <Text style={styles.label}>{label}</Text>
-      <TouchableOpacity onPress={handlePress} disabled={!url}>
-        <Text style={url ? styles.linkValue : styles.value}>{value}</Text>
-      </TouchableOpacity>
+    <View style={styles.contactContainer}>
+      <CustomText style={styles.headerText}>Contact Details</CustomText>
+      <View>
+        <View>
+          <CustomText style={styles.subHeaderText}>Email</CustomText>
+        </View>
+        <CustomText style={styles.subHeaderValueText}>{email}</CustomText>
+      </View>
+      <View>
+        <View>
+          <CustomText style={styles.subHeaderText}>Phone No.</CustomText>
+        </View>
+        <CustomText style={styles.subHeaderValueText}>{phone}</CustomText>
+      </View>
+      <View>
+        <CustomText style={styles.subHeaderText}>Address</CustomText>
+        <CustomText style={styles.subHeaderValueText}>{address}</CustomText>
+      </View>
+      {website && (
+        <View>
+          <CustomText style={styles.subHeaderText}>Website</CustomText>
+          <CustomText
+            style={[styles.subHeaderValueText, { color: COLORS.primary }]}
+          >
+            {website}
+          </CustomText>
+        </View>
+      )}
     </View>
   );
 };
 
-const ContactDetailsCard: React.FC<ContactDetailsCardProps> = ({ details }) => {
-  return (
-    <View style={styles.card}>
-      <Text style={styles.title}>Contact Details</Text>
-      <DetailItem
-        label="Email ID"
-        value={details.email}
-        url={`mailto:${details.email}`}
-      />
-      <DetailItem
-        label="Phone No."
-        value={details.phone}
-        url={`tel:${details.phone}`}
-      />
-      <DetailItem label="Address" value={details.address} />
-      <DetailItem
-        label="Website"
-        value={details.website}
-        url={`https://${details.website}`}
-      />
-    </View>
-  );
-};
+export default ContactDetailsCard;
 
 const styles = StyleSheet.create({
-  card: {
+  contactContainer: {
     backgroundColor: '#fff',
     borderRadius: 12,
-    padding: 20,
+    padding: 16,
+    marginHorizontal: 15,
+    marginVertical: 10,
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.1,
     shadowRadius: 4,
     elevation: 5,
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: '#1f2937',
-    marginBottom: 16,
+  headerText: {
+    fontSize: TEXT_SIZES.lg,
+    fontWeight: '700',
+    color: COLORS.textPrimary,
+    marginTop: 5,
+    marginBottom: 5,
   },
-  itemContainer: {
-    marginBottom: 16,
+  subHeaderText: {
+    fontSize: TEXT_SIZES.md,
+    fontWeight: '700',
+    color: COLORS.textPrimary,
+    marginTop: 10,
   },
-  label: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    color: '#374151',
-  },
-  value: {
-    fontSize: 16,
-    color: '#4b5563',
-  },
-  linkValue: {
-    fontSize: 16,
-    color: '#2563eb',
-    textDecorationLine: 'underline',
+  subHeaderValueText: {
+    fontSize: TEXT_SIZES.sm,
+    fontWeight: '400',
+    color: '#595959',
   },
 });
-
-export default ContactDetailsCard;
