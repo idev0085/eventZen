83b08746373eecf9b39b386a88056d6aad7e04cb
ARAFAT AMAN ALIM 🇮🇳 <76907565+Arafat-alim@users.noreply.github.com>
2025-09-21 18:21:55 +0530

    ✔️Connection detail screen added.

--- src/screens/ConnectionDetails.tsx
+++ src/screens/ConnectionDetails.tsx
@@ -1,49 +1,49 @@
 import React, { useState } from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet, View } from 'react-native';
 import { COLORS } from '../utils/constants';
 import BackHeader from '../components/BackHeader';
 import LoadingOverlay from '../components/loadingOverlay';
 import UserCard from '../components/userCard';
-import ContactDetailsCard from '../components/contactDetailsCard';
 import ContactDetails from '../components/contactDetails';
+import AddNote from '../components/addNote';
+import Button from '../components/ui/button';
 
 const connectionData = {
-  company_name: 'XYZ Corp',
-  image_url:
+  company_name: 'Company Name',
+  avatar:
     'https://w7.pngwing.com/pngs/205/731/png-transparent-default-avatar-thumbnail.png',
-  name: 'Joe Doe',
-  role: 'Senior Director',
+  name: 'Michael Chen',
+  role: 'Additional Director',
   contact_details: {
-    email: 'user@example.com',
-    phone: '8882227771',
-    address: '12C Hong Kong',
-    website: 'www.google.com',
+    email: 'digitalaptech@gmail.com',
+    phone: '+1 7346274598',
+    address:
+      'EN-34 (9th Floor), Block-EN, Sector – V, Salt Lake City, Kolkata – 700091, West Bengal, India.',
+    website: 'digitalaptech.com',
   },
 };
 
 const ConnectionDetails = () => {
   const [isLoading, setIsLoading] = useState(false);
+  const [textArea, setTextArea] = useState('');
 
   if (isLoading) {
     return <LoadingOverlay visible={isLoading} />;
   }
 
-  // if (!dummyData) {
-  //   return null;
-  // }
+  const handleCancel = () => {};
+  const handleSave = () => {};
 
   return (
     <>
       <BackHeader title="Connection Details" showBtn={true} />
       <ScrollView contentContainerStyle={styles.scrollContainer}>
-        {/* <UserCard
-          userName={dummyData?.name}
-          userTitle={dummyData?.role}
-          companyName={dummyData?.company_name}
-          imageUrl={dummyData?.image_url}
-        /> */}
-        {/* <UserList key={'1'} exhibitorsData={exhibitorsData} isSingle={true} /> */}
-        {/* <ContactDetailsCard details={connectionData.contact_details} /> */}
+        <UserCard
+          imageUrl={connectionData.avatar}
+          companyName={connectionData.company_name}
+          name={connectionData.name}
+          designation={connectionData.role}
+        />
         <ContactDetails
           heading="Contact Details"
           email={connectionData.contact_details.email}
@@ -51,6 +51,22 @@ const ConnectionDetails = () => {
           address={connectionData.contact_details.address}
           website={connectionData.contact_details.website}
         />
+        <AddNote
+          heading="Add Note"
+          onChangeText={setTextArea}
+          placeholder="Message"
+        />
+
+        {/* Button Row */}
+        <View style={styles.buttonRow}>
+          <Button
+            title="Cancel"
+            variant="outlined"
+            onPress={handleCancel}
+            style={styles.buttonHalf}
+          />
+          <Button title="Save" onPress={handleSave} style={styles.buttonHalf} />
+        </View>
       </ScrollView>
     </>
   );
@@ -62,29 +78,15 @@ const styles = StyleSheet.create({
   scrollContainer: {
     flexGrow: 1,
     backgroundColor: COLORS.white,
+    paddingBottom: 20,
   },
-  card: {
-    alignSelf: 'center',
-    width: '95%',
-    justifyContent: 'center',
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
-    shadowRadius: 4,
-    elevation: 3,
-    backgroundColor: '#fff',
+  buttonRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     padding: 10,
-    borderRadius: 10,
+    gap: 10,
   },
-  textHeadng: {
-    color: COLORS.text,
-    fontSize: 20,
-    fontFamily: 'Roboto-Bold',
-    marginBottom: 10,
-  },
-  textMeta: {
-    color: COLORS.text,
-    fontSize: 15,
-    fontFamily: 'Roboto-Regular',
+  buttonHalf: {
+    flex: 1,
   },
 });
