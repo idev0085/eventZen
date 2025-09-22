88b2c2282cd838071a2bd957598e7e93119c3724
ARAFAT AMAN ALIM 🇮🇳 <76907565+Arafat-alim@users.noreply.github.com>
2025-09-23 02:06:04 +0530

    ✔️ Connection API added.

---
+++ src/api/connectionsApi.ts
@@ -0,0 +1,62 @@
+import { apiClient } from './apiClient';
+
+export interface IScannedConnection {
+  id: number;
+  name: string;
+  company: string;
+  designation: string;
+  company_website: string;
+  email: string;
+  phone: string;
+  avatar: string;
+  visiting_card_image: string;
+  tags: string; // Comma-separated string
+  rating: 'Cold' | 'Normal' | 'Warm';
+  address: string | null;
+  bio: string;
+  note: string;
+}
+
+export interface ICreateConnectionPayload {
+  title: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  phone: string;
+  company_name: string;
+  job_title: string;
+  address: string;
+  visiting_card_image: string; // base64 string
+  rating: 'Cold' | 'Normal' | 'Warm';
+  tag: string[]; // Array of strings
+  note: string;
+}
+
+// ! API Function
+
+//! SCan a Qr
+export const scanConnection = async (
+  qrData: string,
+): Promise<IScannedConnection> => {
+  console.log(`📡 Scanning with QR Data: ${qrData}`);
+  const { data } = await apiClient.post('/api/connection/scan', { qrData });
+
+  if (!data || !data.id) {
+    throw new Error(data.message || 'Scan Failed!');
+  }
+  return data;
+};
+
+//! Create a connection
+export const createConnection = async (payload: ICreateConnectionPayload) => {
+  console.log('📡 Creating new connection...');
+  const { data } = await apiClient.post('/api/connections/create', payload);
+
+  return data;
+};
+
+//! get a connection details
+export const getConnectionById = async id => {};
+
+//! update note
+export const updateConnectionNote = async payload => {};
